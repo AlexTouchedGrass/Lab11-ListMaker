b3dccd7eb282aeb6d28c65c9500a3a7b37e3775d
AlexTouchedGrass <dean-potts.622237@swl.k12.oh.us>
2024-02-07 09:18:42 -0500

    Words

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList <String> names = new ArrayList<String>();
+        boolean done = false;
+
+
+        do {
+            System.out.println("Welcome to ListMaker, your starting list is :");
+            System.out.println();
+        } while (!done);
     }
 }
--- src/Methods.java
+++ src/Methods.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Methods {
+import java.util.ArrayList;
+import java.util.Arrays;
+public class Methods {
+    ArrayList <String> names = new ArrayList<String>();
+    public void addList(String newString){
+
+    }
 }
