0ccebb2f41328c472eb712906837e679fe21f7ed
AlexTouchedGrass <dean-potts.622237@swl.k12.oh.us>
2024-02-07 09:30:31 -0500

    Words

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,33 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import static java.lang.Thread.sleep;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ArrayList <String> strings = new ArrayList<String>();
         boolean done = false;
         strings.addAll(Arrays.asList("","","","","","","","","",""));
         int listLength = strings.size();
-        String welBack = "";
+        String welcBack = "";
+        String userMenuInput  = "";
+        String userStringInput = "";
 
         do {
-            System.out.println("Welcome " + welBack + "to ListMaker, your starting list is " + listLength + ":");
+            System.out.println("Welcome " + welcBack + "to ListMaker, your starting list is " + listLength + ":");
             System.out.println(strings);
+            sleep(100);
 
+            System.out.println("You can input any of the following: \nA to add an item to the list. \nD to delete an item from the list. \nP to print the list. \nQ to quit the program.");
+
+            if () {
+
+            } else if () {
+
+            } else if () {
+
+            } else if () {
+
+            }
         } while (!done);
     }
 }
