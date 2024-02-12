0e2687e589393de062918e581ec4199960633a82
AlexTouchedGrass <144457672+AlexTouchedGrass@users.noreply.github.com>
2024-02-12 09:27:30 -0500

    Update InputHelper.java

--- src/InputHelper.java
+++ src/InputHelper.java
@@ -1,97 +1,218 @@
-import java.util.ArrayList;
-
-import static java.lang.Thread.sleep;
-
 import java.util.Scanner;
-import java.util.Arrays;
-public class ListMaker {
-    public static void main(String[] args) throws InterruptedException {
+
+public class InputHelper {
+    public static void main(String[] args) {
+        //Test Methods
         Scanner scan = new Scanner(System.in);
 
-        ArrayList<Object> stringsNumbered;
-        ArrayList<Object> strings = new ArrayList<>(Arrays.asList("For", "The", "Emperor", "For", "The", "Corpse", "Emperor", "For", "The", "Weaver"));
-        boolean done = false;
-        String welcBack = "";
-        String userMenuInput = "";
+        //Size Checker
+        int iSize = getInt(scan, "Please enter your size (round to nearest whole number)."); //Results are confidential.
+        System.out.println("Your size is " + iSize + ".");
 
-        do {
-            sleep(1000);
-            int listLength = strings.size();
-            int listRealLength = strings.size() - 1;
-            System.out.println("Welcome " + welcBack + "to ListMaker, your starting list is " + listLength + " Word(s) long:\n");
-            System.out.println(strings);
-            stringsNumbered = strings;
-            sleep(2500); //Feel Free To Delete all Sleeps when you playtest, they probably don't interfere with anything, but they're just to make the appearance of text more spaced out, giving users time to read.
+        //Double Size Checker
+        double dSize = getDouble(scan, "Please enter your size."); //Results are confidential.
+        System.out.println("Your size is " + dSize + ".");
 
-            System.out.println("\nYou can input any of the following, but I recommend starting with P or Print List, it will give you the accurate locations of the words in the list.");
-            sleep(2500);
-            System.out.println("A to add an item to the list. \nD to delete an item from the list. \nP to print the list. \nQ to quit the program.");
-            userMenuInput = scan.nextLine();
-            if (userMenuInput.equalsIgnoreCase("A")) {
-                System.out.println("What would you like to add to the list? Enter Below: ");
-                addList(strings);
-                System.out.println("Your new list is: \n" + strings);
-                welcBack = "back ";
-                sleep(2000);
-            } else if (userMenuInput.equalsIgnoreCase("D")) {
-                System.out.println("Which word would you like taken out of the list?\nPlease enter the index # for the word you want deleted. [0-" + listRealLength + "]");
-                deleteList();
-                welcBack = "back ";
-                sleep(2000);
-            } else if (userMenuInput.equalsIgnoreCase("P")) {
-                System.out.println("Your list will be printed shortly. The numbers next to each word are their actual positioning in the list.\n");
-                sleep(2000);
-                System.out.println(printList(stringsNumbered));
-                System.out.println("\n");
-                welcBack = "back ";
-            } else if (userMenuInput.equalsIgnoreCase("Q")) {
-                System.out.println("Sorry you are feeling this way, that makes me sad.\n");
-                sleep(1500);
-                done = quitList();
-                welcBack = "back ";
-            }
-        } while (!done);
-    }
+        //Range Checker
+        int iRange = getRangedInt(scan,"Please enter an int between 1 and 10.",1,10);
+        System.out.println("Your number is " + iRange + ".");
 
-    //Add Method
-    public static void addList(ArrayList<Object> array){ //EASIEST
-        Scanner scan = new Scanner(System.in);
-        String string = InputHelper.getNonZeroLenString(scan,"Enter a WORD, blank space will not work.");
-        array.add(string);
-    }
+        //Double Range Checker
+        double dRange = getRangedDouble(scan, "Please enter a double between 1 and 10",1.0,10.0);
+        System.out.println("Your number is " + dRange + ".");
 
-    //Delete Method
-    public static void deleteList(){ //HARDEST
+        //Social Security Check
+        String ssn = getRegExString(scan, "Please enter your SSN [xxx-xx-xxxx]", "\\d{3}-\\d{2}-\\d{4}");
+        System.out.println("Your SSN: " + ssn + ".");
+
+        //Date Of Birth Check
+        String dob = getRegExString(scan, "Enter your date of birth [mm.dd.yyyy]","\\d{2}.\\d{2}.\\d{4}");
+        System.out.println("Your date of birth is " + dob + ".");
+
+        //getNonZeroLenString
+        String non0Str = getNonZeroLenString(scan, "Enter a string. (Blank space will not work)");
+        System.out.println("Your string is " + non0Str + ".");
+
+        //getNonZeroInt
+        int non0Int = getNonZeroInt(scan, "Enter an int. (Negatives and 0 are not allowed.)");
+        System.out.println("Your string is " + non0Int + ".");
+
+        //Y or N Confirm
+        String yesOrNo = getYNConfirm(scan, "Would you like to stop? Yes to stop, no to continue. ( Y or N also works)");
+        System.out.println("You answered " + yesOrNo + ".");
 
     }
-    //Print Method
-    public static ArrayList<Object> printList(ArrayList<Object> array){ //Mid took a while to figure out
-        for (int i = 0; i < array.size();i++) {
-            String saveString = i + " - " + array.get(i);
-            array.set(i,saveString);
+
+        //GetInt
+        //This method loops until a valid integer input is received. Returns int value.
+        public static int getInt(Scanner in, String prompt){
+            boolean done = false;
+            int x = 0;
+
+            System.out.println(prompt);
+            do{
+                if(in.hasNextInt()){
+                    x = in.nextInt();
+                    done = true;
+                }
+                in.nextLine(); //buffer
+            } while (!done);
+            return x;
         }
-        return array;
-    }
-    //Quit Method
-    public static boolean quitList(){ //Easy but a lot of code
-        Scanner scan = new Scanner(System.in);
-        boolean done = false;
-        boolean quitListDone = false;
-        System.out.println("Are you sure you would like to quit the ListMaker program? Enter [Y/N]");
-        do {
-            String userDone = scan.nextLine();
-            if (userDone.equalsIgnoreCase("Y")) {
-                System.out.println("Goodbye, you don't know what you're missing.");
-                done = true;
-                quitListDone = true;
-            } else if (userDone.equalsIgnoreCase("N")) {
-                System.out.println("Good to hear, I knew you weren't really done.");
-                quitListDone = true;
-            } else {
-                System.out.println("That is not [Y/N], if you typed the full word that won't work, try again.\n");
-            }
-        } while (!quitListDone);
 
-        return done;
+        //GetDouble
+        //This method loops until a valid double input is received. Returns double value.
+        public static double getDouble(Scanner in, String prompt){
+            boolean done = false;
+            double dGetX = 0;
+
+            System.out.println(prompt);
+            do{
+                if(in.hasNextDouble()){
+                    dGetX = in.nextDouble();
+                    done = true;
+                }
+                in.nextLine(); //buffer
+            } while (!done);
+            return dGetX;
+        }
+
+        //Int Ranged
+        public static int getRangedInt(Scanner in, String prompt, int min, int max){
+            boolean done = false;
+            int x = 0;
+
+            System.out.println(prompt);
+            do{
+                if(in.hasNextInt()){
+                    x = in.nextInt();
+                    //Check to see if number is in range
+                    if (x<= max && x >= min) {
+                        done = true;
+                 }
+            }
+                in.nextLine(); //buffer
+            } while (!done);
+            return x;
+    }
+
+    //RegExString
+    public static String getRegExString(Scanner in, String prompt, String regEx) {
+        boolean done = false;
+        String input;
+
+        System.out.println(prompt);
+        do {
+            input = in.nextLine();
+            if (input.matches(regEx)) {
+                done = true;
+            } else {
+                System.out.println("Invalid Input.");
+            }
+        } while(!done);
+        return input;
+    }
+
+    //Double Ranged
+    public static double getRangedDouble(Scanner in, String prompt, double min, double max){
+        boolean done = false;
+        double dx = 0;
+
+        System.out.println(prompt);
+        do{
+            if(in.hasNextDouble()){
+                dx = in.nextDouble();
+                //Check to see if number is in range
+                if (dx<= max && dx >= min) {
+                    done = true;
+                } else {
+                    System.out.println("Invalid Input.");
+                }
+            }
+            in.nextLine();
+        }while(!done);
+        return dx;
+    }
+
+    public static String getNonZeroLenString(Scanner in, String prompt){
+        boolean done = false;
+        String input;
+
+        System.out.println(prompt);
+        do{
+            input = in.nextLine();
+            if(input.length() > 0){
+                done = true;
+                } else {
+                System.out.println("Error. Blank Space Detected.");
+            }
+             // in.nextLine();
+        }while(!done);
+        return input;
+    }
+
+    //Checks that the entered Int is greater than 0 (Only for positive numbers)
+    public static int getNonZeroInt(Scanner in, String prompt){
+        boolean done = false;
+        int input;
+
+        System.out.println(prompt);
+        do{
+            input = in.nextInt();
+            if(input > 0){
+                done = true;
+            } else {
+                System.out.println("Error. Either a negative integer or 0 has been entered.");
+            }
+            in.nextLine();
+        }while(!done);
+        return input;
+    }
+
+    public static String getYNConfirm(Scanner in, String prompt){
+        boolean done = false;
+        String input;
+
+        System.out.println(prompt);
+        do{
+            input = in.nextLine();
+            if(input.equalsIgnoreCase("yes") || input.equalsIgnoreCase("y")){
+                System.out.println("You will stop.");
+                done = true;
+            } else if (input.equalsIgnoreCase("no") || input.equalsIgnoreCase("n")) {
+                System.out.println("You will continue.");
+                done = true;
+            }
+             // in.nextLine();
+        }while(!done);
+        return input;
+    }
+
+    //PRETTY HEADER
+    public static void getPrettyHeader(String msg) {
+        int leftSpaces = ((54-msg.length())/2);
+        int rightSpaces = (54-leftSpaces-msg.length());
+
+        printCharacter("*", 60, true);
+
+        printCharacter("*",3, false);
+        printCharacter(" ", leftSpaces, false);
+        System.out.print(msg);
+        printCharacter(" ", rightSpaces, false);
+        printCharacter("*", 3, true);
+
+        printCharacter("*", 60, false);
+    }
+
+
+        //PRINT CHARACTER (MADE FOR PRETTY HEADER)
+    public static void printCharacter(String character, int amount, boolean newLine) {
+        for (int i = 0; i < amount; i++) {
+            System.out.print(character);
+        }
+
+        if (newLine) {
+            System.out.println();
+        }
     }
 }
