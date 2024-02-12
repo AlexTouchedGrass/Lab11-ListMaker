397cb87c5cca432371f13ea6e7b78122707bede0
AlexTouchedGrass <144457672+AlexTouchedGrass@users.noreply.github.com>
2024-02-12 09:24:26 -0500

    Update ListMaker.java

--- src/ListMaker.java
+++ src/ListMaker.java
@@ -5,10 +5,11 @@ import static java.lang.Thread.sleep;
 import java.util.Scanner;
 import java.util.Arrays;
 public class ListMaker {
-    public static void main(String[] args) throws InterruptedException {
-        Scanner scan = new Scanner(System.in);
+    public static Scanner scan = new Scanner(System.in);
 
-        ArrayList<Object> stringsNumbered;
+    public static void main(String[] args) throws InterruptedException {
+
+
         ArrayList<Object> strings = new ArrayList<>(Arrays.asList("For", "The", "Emperor", "For", "The", "Corpse", "Emperor", "For", "The", "Weaver"));
         boolean done = false;
         String welcBack = "";
@@ -20,7 +21,6 @@ public class ListMaker {
             int listRealLength = strings.size() - 1;
             System.out.println("Welcome " + welcBack + "to ListMaker, your starting list is " + listLength + " Word(s) long:\n");
             System.out.println(strings);
-            stringsNumbered = strings;
             sleep(2500); //Feel Free To Delete all Sleeps when you playtest, they probably don't interfere with anything, but they're just to make the appearance of text more spaced out, giving users time to read.
 
             System.out.println("\nYou can input any of the following, but I recommend starting with P or Print List, it will give you the accurate locations of the words in the list.");
@@ -35,13 +35,13 @@ public class ListMaker {
                 sleep(2000);
             } else if (userMenuInput.equalsIgnoreCase("D")) {
                 System.out.println("Which word would you like taken out of the list?\nPlease enter the index # for the word you want deleted. [0-" + listRealLength + "]");
-                deleteList();
+                deleteList(strings);
                 welcBack = "back ";
                 sleep(2000);
             } else if (userMenuInput.equalsIgnoreCase("P")) {
                 System.out.println("Your list will be printed shortly. The numbers next to each word are their actual positioning in the list.\n");
                 sleep(2000);
-                System.out.println(printList(stringsNumbered));
+                System.out.println(printList(strings));
                 System.out.println("\n");
                 welcBack = "back ";
             } else if (userMenuInput.equalsIgnoreCase("Q")) {
@@ -55,26 +55,28 @@ public class ListMaker {
 
     //Add Method
     public static void addList(ArrayList<Object> array){ //EASIEST
-        Scanner scan = new Scanner(System.in);
         String string = InputHelper.getNonZeroLenString(scan,"Enter a WORD, blank space will not work.");
         array.add(string);
     }
 
     //Delete Method
-    public static void deleteList(){ //HARDEST
+    public static void deleteList(ArrayList<Object> array){ //HARDEST
+        int ranges = InputHelper.getRangedInt(scan,"If you aren't sure what the index is maybe try Printing the List.",0,array.size());
 
+        array.remove(ranges);
+        System.out.println("Your new list is : \n" + array);
     }
     //Print Method
-    public static ArrayList<Object> printList(ArrayList<Object> array){ //Mid took a while to figure out
+    public static String printList(ArrayList<Object> array){ //Mid took a while to figure out
+        StringBuilder saveString = new StringBuilder();
         for (int i = 0; i < array.size();i++) {
-            String saveString = i + " - " + array.get(i);
-            array.set(i,saveString);
+            saveString.append(" \n").append(i).append(" - ").append(array.get(i)).append(" ");
+
         }
-        return array;
+        return saveString.toString();
     }
     //Quit Method
     public static boolean quitList(){ //Easy but a lot of code
-        Scanner scan = new Scanner(System.in);
         boolean done = false;
         boolean quitListDone = false;
         System.out.println("Are you sure you would like to quit the ListMaker program? Enter [Y/N]");
